35bd0071ac2354cb84bd51f20e880acbfa5ebb91
Michael Brooks <michael@michaelbrooks.ca>
2013-03-02 22:54:30 -0800

    [test] Use single quotes for consistency.

--- test/main.js
+++ test/main.js
@@ -1,12 +1,12 @@
-var expect = require("expect.js"),
+var expect = require('expect.js'),
     qrcode = require('./../lib/main'),
-    sinon = require("sinon");
+    sinon = require('sinon');
 
 describe('in the main module', function() {
-    describe("the generate method", function () {
-        describe("when not providing a callback", function () {
+    describe('the generate method', function () {
+        describe('when not providing a callback', function () {
             beforeEach(function () {
-                sinon.stub(console, "log");
+                sinon.stub(console, 'log');
             });
 
             afterEach(function () {
@@ -14,35 +14,35 @@ describe('in the main module', function() {
                 console.log.reset();
             });
 
-            it("logs to the console", function () {
-                qrcode.generate("test");
+            it('logs to the console', function () {
+                qrcode.generate('test');
                 expect(console.log.called).to.be(true);
             });
         });
 
-        describe("when providing a callback", function () {
-            it("will call the callback", function () {
+        describe('when providing a callback', function () {
+            it('will call the callback', function () {
                 var cb = sinon.spy();
-                qrcode.generate("test", cb);
+                qrcode.generate('test', cb);
                 expect(cb.called).to.be(true);
             });
 
-            it("will not call the console.log method", function () {
-                qrcode.generate("test", sinon.spy());
+            it('will not call the console.log method', function () {
+                qrcode.generate('test', sinon.spy());
                 expect(console.log.called).to.be(false);
             });
         });
 
-        describe("the QR Code", function () {
-            it("should be a string", function (done) {
-                qrcode.generate("test", function(result) {
+        describe('the QR Code', function () {
+            it('should be a string', function (done) {
+                qrcode.generate('test', function(result) {
                     expect(result).to.be.a('string');
                     done();
                 });
             });
 
-            it("should not end with a newline", function (done) {
-                qrcode.generate("test", function(result) {
+            it('should not end with a newline', function (done) {
+                qrcode.generate('test', function(result) {
                     expect(result).not.to.match(/\n$/);
                     done();
                 });
