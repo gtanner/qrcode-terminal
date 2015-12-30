bb21ae97dec1f8434c0b4fa16e05fdf86be69a6d
Stefan Schüller <sschueller@techdroid.com>
2015-12-30 15:56:03 +0100

    Added option for error correction
    error correction can be set with -e or --errorcorrectlevel=

--- bin/qrcode-terminal.js
+++ bin/qrcode-terminal.js
@@ -6,20 +6,21 @@
 
 var qrcode = require('../lib/main'),
     path = require('path'),
-    fs = require('fs');
+    fs = require('fs'),
+    parseArgs = require('minimist');
 
 /*!
  * Parse the process name and input
  */
+var argv = parseArgs(process.argv, opts={});
 
-var name = process.argv[1].replace(/^.*[\\\/]/, '').replace('.js', ''),
-    input = process.argv[2];
+var name = argv._[1].replace(/^.*[\\\/]/, '').replace('.js', ''),
+    input = argv._[2];
 
 /*!
  * Display help
  */
-
-if (!input || input === '-h' || input === '--help') {
+if (typeof argv.h !== 'undefined' || typeof argv.help !== 'undefined') {
     help();
     process.exit();
 }
@@ -27,17 +28,26 @@ if (!input || input === '-h' || input === '--help') {
 /*!
  * Display version
  */
-
-if (input === '-v' || input === '--version') {
+if (typeof argv.v !== 'undefined' || typeof argv.version !== 'undefined') {
     version();
     process.exit();
 }
 
 /*!
+ * Error correction level
+ */
+if (typeof argv.e !== 'undefined' && typeof argv.e === 'string') {
+    qrcode.setErrorLevel(argv.e.toUpperCase());
+} else if (typeof argv.errorcorrectlevel !== 'undefined' && typeof argv.errorcorrectlevel === 'string') {
+    qrcode.setErrorLevel(argv.errorcorrectlevel.toUpperCase());
+}
+
+/*!
  * Render the QR Code
  */
-
-qrcode.generate(input);
+if (typeof input !== 'undefined') {
+    qrcode.generate(input);
+}
 
 /*!
  * Helper functions
@@ -49,13 +59,15 @@ function help() {
         'Usage: ' + name + ' <message>',
         '',
         'Options:',
-        '  -h, --help           output usage information',
-        '  -v, --version        output version number',
+        '  -e [L|M|Q|H], --errorcorrectlevel=[L|M|Q|H]   set error correction level (Default: L)',
+        '  -h, --help                                    output usage information',
+        '  -v, --version                                 output version number',
         '',
         'Examples:',
         '',
         '  $ ' + name + ' hello',
         '  $ ' + name + ' "hello world"',
+        '  $ ' + name + ' -e Q "hello world"',
         ''
     ].join('\n'));
 }
